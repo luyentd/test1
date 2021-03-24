c9d618213722b0e5f04c1f7ed7633bb852faa924
luyentd <37621024+luyentd@users.noreply.github.com>
2021-03-24 14:22:20 +0700

    Create ab2.py

---
+++ ab2.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+class PersonAbstact(ABC):
+    name = None
+    age = 0
+    def getName(self):
+        print(self.name)
+    def getAge(self):
+        print(self.age)
+    @abstractmethod
+    def getFull(self):
+        pass
+
+class Person(PersonAbstact):
+    name = 'Vu Thanh Tai'
+    age = 22
+    def getFull(self):
+        self.getName()
+        self.getAge()
+
+Person().getFull();
