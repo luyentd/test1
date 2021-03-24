1d7defd0656d29741ac7ec878b0eb1385840d041
luyentd <37621024+luyentd@users.noreply.github.com>
2021-03-24 14:20:11 +0700

    Create abstractClass.py

---
+++ abstractClass.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class PersonAbstact(ABC):
+    name = None
+    age = 0
+    def getName(self):
+        print(self.name)
+    def getAge(self):
+        print(self.age)
+    @abstractmethod
+    def getFull(self):
+        pass
+
+class Person(PersonAbstact):
+    pass
+    
+Person();
